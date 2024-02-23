d5244c828e41703503ce0e83a8bf2a4a70b17564
dev <mpls@posteo.net>
2024-02-23 11:58:22 -0600

    add license

--- src/leveldb.rs
+++ src/leveldb.rs
@@ -1,3 +1,18 @@
+// Copyright (C) 2024      Whittier Digital Technologies LLC
+//
+// This file is part of silent-payement-indexer.
+//
+// silent-payment-indexer is free software: you can redistribute it and/or modify it under the terms of the
+// GNU General Public License as published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// silent-payment-indexer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with Foobar. If not, see
+// <https://www.gnu.org/licenses/>.
+
 use leveldb::database::Database;
 use leveldb::kv::KV;
 use leveldb::options::{Options, ReadOptions, WriteOptions};
