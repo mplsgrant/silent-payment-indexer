5ec9f76f8f5eb2ae4fb6b5ece3bc9c208c7949a2
dev <mpls@posteo.net>
2024-02-25 09:38:24 -0600

    do p2wpkh, start p2tr

--- src/lib.rs
+++ src/lib.rs
@@ -48,6 +48,9 @@ fn get_pubkey_from_input(vin: InputData) {
     if vin.prevout.is_p2sh() {
         get_pubkey_from_p2sh_p2wpkh(&vin);
     }
+    if vin.prevout.is_p2wpkh() {
+        get_pubkey_from_p2wpkh(&vin);
+    }
 }
 
 fn get_pubkey_from_p2pkh(vin: &InputData) -> Option<PublicKey> {
@@ -92,6 +95,27 @@ fn get_pubkey_from_p2sh_p2wpkh(vin: &InputData) -> Option<PublicKey> {
     }
 }
 
+fn get_pubkey_from_p2wpkh(vin: &InputData) -> Option<PublicKey> {
+    vin.txinwitness
+        .last()
+        .and_then(|maybe_pubkey| PublicKey::from_slice(maybe_pubkey).ok())
+        .filter(|pub_key| pub_key.compressed)
+}
+
+fn get_pubkey_from_p2tr(vin: &InputData) -> Option<PublicKey> {
+    // TODO Why does tapscript() give us Script when BIP 141 says that "Witness data is NOT script."
+    let y = vin.txinwitness.len();
+    println!("{y}");
+    let z = vin
+        .txinwitness
+        .tapscript().map(|witness| witness.instruction_indices()).and_then(|instruction_indices| instruction_indices);
+    println!("z: {z:?}");
+    z.and_then(|witness| Some(println!("witness: {witness:?}")));
+
+
+    None
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -134,6 +158,35 @@ mod tests {
             txinwitness: deserialize::<Witness>(&hex!("02483046022100ad79e6801dd9a8727f342f31c71c4912866f59dc6e7981878e92c5844a0ce929022100fb0d2393e813968648b9753b7e9871d90ab3d815ebf91820d704b19f4ed224d621025a1e61f898173040e20616d43e9f496fba90338a39faa1ed98fcbaeee4dd9be5")).unwrap(),
         };
         let maybe_pubkey = get_pubkey_from_p2sh_p2wpkh(&vin);
+        assert_eq!(
+            maybe_pubkey.unwrap().pubkey_hash().to_string(),
+            "19c2f3ae0ca3b642bd3e49598b8da89f50c14161"
+        );
+    }
+    #[test]
+    fn simple_get_pubkey_from_p2wpkh() {
+        // TODO got these values from they p2sh_p2wpkh test. Need to find (or make) known-good p2wpkh test data.
+        let vin = InputData {
+            prevout: ScriptBuf::from_hex("00140423f731a07491364e8dce98b7c00bda63336950").unwrap(),
+            script_sig: ScriptBuf::new(),
+
+            txinwitness: deserialize::<Witness>(&hex!("02483046022100ad79e6801dd9a8727f342f31c71c4912866f59dc6e7981878e92c5844a0ce929022100fb0d2393e813968648b9753b7e9871d90ab3d815ebf91820d704b19f4ed224d621025a1e61f898173040e20616d43e9f496fba90338a39faa1ed98fcbaeee4dd9be5")).unwrap(),
+        };
+        let maybe_pubkey = get_pubkey_from_p2wpkh(&vin);
+        assert_eq!(
+            maybe_pubkey.unwrap().pubkey_hash().to_string(),
+            "19c2f3ae0ca3b642bd3e49598b8da89f50c14161"
+        );
+    }
+    #[test]
+    fn simple_get_pubkey_from_p2tr() {
+        let vin = InputData {
+            prevout: ScriptBuf::from_hex("51205a1e61f898173040e20616d43e9f496fba90338a39faa1ed98fcbaeee4dd9be5").unwrap(),
+            script_sig: ScriptBuf::new(),
+
+            txinwitness: deserialize::<Witness>(&hex!("0440c459b671370d12cfb5acee76da7e3ba7cc29b0b4653e3af8388591082660137d087fdc8e89a612cd5d15be0febe61fc7cdcf3161a26e599a4514aa5c3e86f47b22205a1e61f898173040e20616d43e9f496fba90338a39faa1ed98fcbaeee4dd9be5ac21c150929b74c1a04954b78b4b6035e97a5e078a5a0f28ec96d547bfee9ace803ac00150")).unwrap(),
+        };
+        let maybe_pubkey = get_pubkey_from_p2tr(&vin);
         assert_eq!(
             maybe_pubkey.unwrap().pubkey_hash().to_string(),
             "19c2f3ae0ca3b642bd3e49598b8da89f50c14161"
